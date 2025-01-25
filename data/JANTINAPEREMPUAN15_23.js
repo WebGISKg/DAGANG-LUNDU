ce652c7cce14817ff4b3545c176c707223552d86
WebGISKg <wardawardina2@gmail.com>
2025-01-25 12:00:54 +0800

    Add files via upload

---
+++ data/JANTINAPEREMPUAN15_23.js
@@ -0,0 +1,1 @@
+var json_JANTINAPEREMPUAN15_23 = {"type":"FeatureCollection","name":"JANTINAPEREMPUAN15_23","crs":{"type":"name","properties":{"name":"urn:ogc:def:crs:OGC:1.3:CRS84"}},"features":[]}
